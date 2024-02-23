8f0998bf20e01bdf932f2d6f87933b18fe261637
Avishka <avishkakapuruge@gmail.com>
2024-02-23 21:15:53 +0530

    rotating-blocks

--- sketch.js
+++ sketch.js
@@ -1,7 +1,36 @@
+
+let distMouse = 15;
+let cols;
+let rows;
+let blocks = [];
+let offset = 4;
+let size = 10;
+
 function setup() {
     createCanvas(400, 400);
+    rectMode(CENTER);
+    angleMode(DEGREES);
+
+    cols = width / size;
+    rows = height / size;
+
+    for (let i = 0; i < cols; i++) {
+        blocks[i] = [];
+        for (let j = 0; j < rows; j++) {
+            blocks[i][j] = new Block(size / 2 + i * size, size / 2 + j * size);
+        }
+    }
+
+    b = new Block(width / 2, height / 2);
 }
 
 function draw() {
-    background(220);
+    background(0);
+    for (let i = 0; i < cols; i++) {
+        for (let j = 0; j < rows; j++) {
+            blocks[i][j].move();
+            blocks[i][j].display();
+        }
+    }
+
 }
